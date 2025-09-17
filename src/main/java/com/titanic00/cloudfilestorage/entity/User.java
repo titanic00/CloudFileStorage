3cc10839a116b9f21b8fd385295813362dbdbe83
Vitaliy_Mi <vitalii.mishchenko@cec.valantic.com>
2025-09-17 16:33:09 +0200

    create user entity, create connection to database

---
+++ src/main/java/com/titanic00/cloudfilestorage/entity/User.java
@@ -0,0 +1,25 @@
+package com.titanic00.cloudfilestorage.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
