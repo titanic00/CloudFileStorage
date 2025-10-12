67852503250f2fb656e267baca4d79622b444d5f
Vitaliy_Mi <vitalii.mishchenko@cec.valantic.com>
2025-10-12 22:36:20 +0200

    first commit

---
+++ src/main/java/com/titanic00/cloudfilestorage/CloudFileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.titanic00.cloudfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFileStorageApplication.class, args);
+	}
+
+}
